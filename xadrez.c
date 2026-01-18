f531f04740f36573adc4cbceb3df73a7c11f1675
Vinnyoff <marcosvf2000@gmail.com>
2026-01-17 21:15:43 -0400

    nivel intermediario

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,7 @@ int main()
 {
 
     // Declaração de variáveis para movimentos
-    int torre, bispo, rainha = 0;
+    int torre = 0, bispo = 0, rainha = 0, cavalo = 0;
 
     // Mensagem de inicialização do programa
     printf("Movimentos das Peças de Xadrez:\n");
@@ -12,7 +12,7 @@ int main()
     printf("Movimentos do Torre: \n");
 
     // Loop para simular os movimentos da torre
-    for (int torre = 0; torre < 5; torre++)
+    for (torre = 0; torre < 5; torre++)
     {
         printf("Direita \n");
     }
@@ -38,6 +38,23 @@ int main()
         printf("Esquerda \n");
         rainha++;
     } while (rainha < 8);
+    printf("------------------------------\n");
+
+    // Mensagem para movimentos do cavalo
+    printf("Movimentos do Cavalo: \n");
+
+    // Loop aninhado para simular os movimentos do cavalo
+    int i, j = 0; // Declaração de variáveis de controle
+
+    for (i = 0; i < 1; i++) // Loop externo
+    {
+        while (j < 2) // Loop interno
+        {
+            printf("Cima\n");
+            j++;
+        }
+        printf("Direita\n");
+    }
 
     // Finalização do programa
     printf("------------------------------\n");
